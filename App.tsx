604cb0bfce4e6bb33ccf5ccc98ee4b2a93049075
Guicha-t <thomas.guichard@epitech.eu>
2020-02-13 15:15:51 +0100

    FIrst commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import HelloText from "./src/components/HelloText";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HelloText
+          text="Open up App.js to start working on your app!"
+          color="#000"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ src/components/HelloText.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+
+interface Props {
+  text: string;
+  color: string;
+}
+
+export default ({ text, color }: Props) => (
+  <Text style={{ color }}>{text}</Text>
+);
