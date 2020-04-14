04b5e421b5a41c66856fdd98aea9f5a2f42d14c9
Guicha-t <thomas.guichard@epitech.eu>
2020-04-14 17:24:23 +0200

    Profil Page Login CreateAccount

--- src/Screen/Login.js
+++ src/Screen/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, Image, Dimensions, StyleSheet, FlatList } from 'react-native';
+import { View, TouchableOpacity, Text, Image, Dimensions, StyleSheet, FlatList, TextInput, Button, Alert} from 'react-native';
 
 import StyleWrapper from '../HOC/styleHOC';
 
@@ -12,9 +12,80 @@ function Login({loading}) {
 
   }
 
+  const [email, onChangeEmail] = React.useState('thomas');
+  const [pwd, onChangePwd] = React.useState('bonjour');
+
+  function FetchLogIn() {
+      fetch('http://193.70.90.162:3000/auth/login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: pwd,
+        }),
+      }).then(response => {
+           const statusCode = response.status;
+           if (statusCode == 200) {
+             const data = response.json();
+             return Promise.all([statusCode, data]);
+           } else {
+             return Promise.all([statusCode]);
+           }
+         })
+         .then(([res, data]) => {
+           console.log(res, data);
+           if (data == null) {
+             Alert.alert("Echec lors de la création du compte")
+           } else {
+             Alert.alert(JSON.stringify(data))
+           }
+         })
+         .catch(error => {
+           console.error(error);
+           return { name: "network error", description: "" };
+         });
+       }
+
   return (
     <View style={{ flex: 1, justifyContent: 'flex-start'}}>
-      <Text>Login</Text>
+
+      <View style={{flex: 0.4, alignItems:'center', justifyContent:'center'}}>
+        <Text>LOGO</Text>
+      </View>
+
+      <View style={{flex: 0.6, padding: 20}}>
+
+
+        <View style={{flex: 0.2, paddingBottom: 20}}>
+            <Text style={{fontSize: 18}}>Email</Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, backgroundColor:'white'}}
+              onChangeText={text => onChangeEmail(text)}
+              value={email}
+            />
+        </View>
+        <View style={{flex: 0.2, paddingBottom: 20}}>
+            <Text style={{fontSize: 18}}>Mot de passe</Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, backgroundColor:'white'}}
+              onChangeText={text => onChangePwd(text)}
+              value={pwd}
+            />
+        </View>
+
+        <View style={{flex: 0.6, alignItems:'center', justifyContent:'center'}}>
+          <Button
+            onPress={() => FetchLogIn()}
+            title="Se connecter"
+            color='orange'
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+
+      </View>
     </View>
   );
 }
--- src/Screen/Profile.js
+++ src/Screen/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, Image, Dimensions, StyleSheet, FlatList } from 'react-native';
+import { View, TouchableOpacity, Text, Image, Dimensions, StyleSheet, FlatList, Alert, TextInput, Button} from 'react-native';
 
 import StyleWrapper from '../HOC/styleHOC';
 
 import LoadingIcon from '../components/LoadingIcon';
+
+const HEIGHT = Dimensions.get('window').height;
+const WIDTH = Dimensions.get('window').width;
 
 function Profile({loading}) {
   if (loading) {
@@ -12,9 +15,80 @@ function Profile({loading}) {
 
   }
 
+  const [nom, onChangeNom] = React.useState('New Nom');
+  const [prenom, onChangePrenom] = React.useState('New Prenom');
+  const [email, onChangeEmail] = React.useState('New Email');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'flex-start'}}>
-      <Text>Profile</Text>
+    <View style={{ flex: 1, justifyContent: 'flex-start', paddingTop: 12}}>
+      <View style={{ flex: 0.4, backgroundColor: '#ffc848', alignItems: 'center', justifyContent:'center'}}>
+        <Image
+          source={{ uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png'}}
+          style={{width: WIDTH/2, height: WIDTH/2, borderRadius: WIDTH/4}}
+        />
+        <TouchableOpacity style={{position: 'absolute', right: 0, top: 0, padding: 20, }} onPress={() => Alert.alert("Hello")}>
+          <Image
+              source={require('./../../assets/pen.png')}
+              style={{width: 40, height: 40}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={{position: 'absolute', left: 0, top: 0, padding: 20, }} onPress={() => Alert.alert("Hello")}>
+          <Image
+              source={require('./../../assets/logout.png')}
+              style={{width: 40, height: 40}}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={{flex: 0.5, padding: 20}}>
+
+          <View style={{flex: 0.3, paddingBottom: 20}}>
+            <View style={{flex: 0.4}}>
+              <Text style={{fontSize: 18}}>Prénom</Text>
+            </View>
+            <View style={{flex: 0.6}}>
+              <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, backgroundColor:'white'}}
+                onChangeText={text => onChangeNom(text)}
+                value={nom}
+              />
+            </View>
+          </View>
+
+          <View style={{flex: 0.3, paddingBottom: 20}}>
+            <View style={{flex: 0.4}}>
+              <Text style={{fontSize: 18}}>Nom</Text>
+            </View>
+            <View style={{flex: 0.6}}>
+              <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, backgroundColor:'white' }}
+                onChangeText={text => onChangePrenom(text)}
+                value={prenom}
+              />
+            </View>
+          </View>
+
+          <View style={{flex: 0.3, paddingBottom: 20}}>
+            <View style={{flex: 0.4}}>
+              <Text style={{fontSize: 18}}>Email</Text>
+            </View>
+            <View style={{flex: 0.6}}>
+              <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, backgroundColor:'white' }}
+                onChangeText={text => onChangeEmail(text)}
+                value={email}
+              />
+          </View>
+
+        </View>
+      </View>
+      <View style={{flex: 0.1, justifyContent:'center', alignItems:'center'}}>
+        <Button
+          onPress={() => Alert.alert("ok")}
+          title="Enregistrer"
+          color='orange'
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
     </View>
   );
 }
